ba2a384fcdf617d5a0a111e8edad4476bb30d173
Long <richnesspea2504@gmail.com>
2024-08-01 07:57:37 +0700

    004_11.c

--- 004_array/004_10.c
+++ 004_array/004_10.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
+#define SIZE 7
+//Write a function that receives an array. 
+//The function should find and return the "Second smallest" value in the array
 
+int secondSmallest(int *arr, int size){
+    int smallestValue = arr[0];
+    int maxValue = arr[0];
+    int secondSmallestValue = arr[0];
+    for(int i = 1; i < size; i++){
+        if(arr[i] < smallestValue){
+            smallestValue = arr[i];
+        }
+        if(arr[i] > maxValue){
+            maxValue = arr[i];
+        }
+    }
 
+    printf("the smallest value is %d \n", smallestValue);
+    printf("the max value is %d \n", maxValue);
+
+    secondSmallestValue = maxValue;
+
+    for(int j = 0; j < size; j++){
+        if(arr[j] > smallestValue && arr[j] < secondSmallestValue){
+            secondSmallestValue = arr[j];
+        }
+    }
+    printf("the second smallest value is %d \n", secondSmallestValue);
+    return secondSmallestValue;
+}
 
 int main(){
-
+    int arr[SIZE] = {78, 2, 3, 4, 5, 6, 7};
+    int result = secondSmallest(arr, SIZE);
+    printf("%d \n",result);
 }
---
+++ 004_array/004_11.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//Write a function that receives a sorted (smallest-> largest) array of elements.
+//The function also receives a value and check if there is a sum of 2 elements in the array that equals to received value.
+//if yes return 1 and pass indexes, if no return 0
+
+int main(){
+
+}
